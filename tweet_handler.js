7ffc8886a7b9bcb6a2357bd7a9adece08ca47712
Abhi Gulati <abhigulati@gmail.com>
2015-05-23 14:36:03 -0500

    Moved code to separate js file, added style.css

---
+++ tweet_handler.js
@@ -0,0 +1,31 @@
+$(document).ready(function() {
+        
+  var $body = $('body');
+  var index = streams.home.length - 1;
+  while(index >= 0){
+    var tweet = streams.home[index];
+    var $tweet = $('<div></div>');
+    $tweet.text('@' + tweet.user + ': ' + tweet.message);
+    $tweet.addClass("tweet");
+    $tweet.appendTo($body);
+    index -= 1;
+  }
+});
+
+$("button").on("click", function() {
+  var numDisplayedTweets = $(".tweet").length;
+  if(numDisplayedTweets < streams.home.length) {
+    console.log(numDisplayedTweets + "," + streams.home.length);
+    var index = numDisplayedTweets;
+    while(index < streams.home.length) {
+      var $body = $('body');
+      var tweet = streams.home[index];
+      var $tweet = $('<div></div>');
+      $tweet.text('@' + tweet.user + ': ' + tweet.message);
+      $tweet.addClass("tweet");
+      $(".tweet").first().before($tweet);
+      console.log($(".tweet").first());
+      index += 1;
+    }
+  }
+});
