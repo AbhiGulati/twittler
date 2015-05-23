ad0c0a4e5ce6fd003e3df452cd96147f2d932b02
Abhi Gulati <abhigulati@gmail.com>
2015-05-23 14:58:46 -0500

    Removed some lines that were for debugging

--- tweet_handler.js
+++ tweet_handler.js
@@ -15,10 +15,8 @@ $(document).ready(function() {
 
 
 function refreshTweetDisplay() {
-  alert("button clicked");
   var numDisplayedTweets = $(".tweet").length;
   if(numDisplayedTweets < streams.home.length) {
-    console.log(numDisplayedTweets + "," + streams.home.length);
     var index = numDisplayedTweets;
     while(index < streams.home.length) {
       var $body = $('body');
@@ -27,7 +25,6 @@ function refreshTweetDisplay() {
       $tweet.text('@' + tweet.user + ': ' + tweet.message);
       $tweet.addClass("tweet");
       $(".tweet").first().before($tweet);
-      console.log($(".tweet").first().text());
       index += 1;
     }
   }
